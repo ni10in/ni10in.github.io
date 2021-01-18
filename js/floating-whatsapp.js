aff2e4f56a4eee19c6b724dd4e535679a57e777b
Ni10in <ni10in10@gmail.com>
2021-01-19 00:41:24 +0530

    whatsapp

---
+++ js/floating-whatsapp.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('#whatsAppDiv').floatingWhatsApp({
+        phone: '+918826213408',
+        popupMessage: 'Hello, how can i help you?',
+        message: "please share history project",
+        headerTitle: 'Welcome!',
+        position: 'right',
+        showPopup: true
+    });
+});
